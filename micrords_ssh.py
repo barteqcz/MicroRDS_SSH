e7512949449236bf98b1731b621259acf04996d6
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-03-01 00:01:24 +0100

    Update micrords_ssh.py

--- micrords_ssh.py
+++ micrords_ssh.py
@@ -1,10 +1,16 @@
 import os
-import sys
+from sys import exit, executable
 from paramiko import AutoAddPolicy, SSHClient
 from configparser import ConfigParser
-import time
+from time import sleep
 
-def connect_to_ssh(address, username, password, port):
+def closeAll():
+    print("Closing the screen session and connection...")
+    commandRunning(ssh_client, 'screen -S MicroRDS -X quit')
+    ssh_client.close()
+    exit()
+
+def sshConnection(address, username, password, port):
     ssh_client = SSHClient()
     ssh_client.set_missing_host_key_policy(AutoAddPolicy())
     
@@ -14,34 +20,34 @@ def connect_to_ssh(address, username, password, port):
         return ssh_client
 
     except Exception as e:
-        print(f"Error connecting to the SSH server: {e}")
+        print(f"Error: Connection to the SSH server failed: {e}")
         return None
 
-def run_command(ssh_client, command):
+def commandRunning(ssh_client, command):
     stdin, stdout, stderr = ssh_client.exec_command(command)
     print(stdout.read().decode())
     print(stderr.read().decode())
 
-def write_to_fifo(ssh_client, fifo_path, data):
+def fifoWriting(ssh_client, fifo_path, data):
     try:
         channel = ssh_client.get_transport().open_session()
         channel.exec_command(f'echo -e "{data}\\n" > {fifo_path}')
         channel.shutdown_write()
 
     except Exception as e:
-        print(f"Error writing to FIFO file: {e}")
+        print(f"Error: Writing to FIFO file not possible: {e}")
 
-def send_commands_from_file(ssh_client, fifo_path, local_file_path):
+def fileCommands(ssh_client, fifo_path, source_path):
     try:
-        prev_mod_time = os.path.getmtime(local_file_path)
+        prev_mod_time = os.path.getmtime(source_path)
         prev_content = ""
         
         while True:
-            curr_mod_time = os.path.getmtime(local_file_path)
+            curr_mod_time = os.path.getmtime(source_path)
             
             if curr_mod_time > prev_mod_time:
                 prev_mod_time = curr_mod_time
-                with open(local_file_path, 'r') as file:
+                with open(source_path, 'r') as file:
                     new_content = file.read()
                     if new_content != prev_content:
                         prev_content = new_content
@@ -49,46 +55,53 @@ def send_commands_from_file(ssh_client, fifo_path, local_file_path):
                         for line in lines:
                             line = line.strip()
                             if line:
-                                write_to_fifo(ssh_client, fifo_path, line)
-            time.sleep(1)
+                                fifoWriting(ssh_client, fifo_path, line)
+            sleep(1)
 
-    except KeyboardInterrupt:
-        print("Closing the screen session and connection...")
-        run_command(ssh_client, 'screen -S MicroRDS -X quit')
-        ssh_client.close()
     except FileNotFoundError:
-        print(f"Error: File '{local_file_path}' not found.")
+        print(f"Error: File '{source_path}' not found.")
+        closeAll()
+
     except Exception as e:
-        print(f"Error reading or sending commands: {e}")
+        print(f"Error: Reading or sending commands not possible: {e}")
+        closeAll()
 
 if __name__ == "__main__":
-    # Getting the directory of the executable file
-    exe_dir = os.path.dirname(sys.executable)
-    config_path = os.path.join(exe_dir, 'config.conf')
+    try:
+        exe_dir = os.path.dirname(executable)
+        config_path = os.path.join(exe_dir, 'config.conf')
 
-    config = ConfigParser()
-    config.read(config_path)
+        config = ConfigParser()
+        config.read(config_path)
 
-    ssh_config = config['SSH']
-    settings = config['Settings']
+        ssh_config = config['SSH']
+        settings = config['Settings']
 
-    address = ssh_config.get('address')
-    port = ssh_config.getint('port')
-    username = ssh_config.get('username')
-    password = ssh_config.get('password')
+        address = ssh_config.get('address')
+        port = ssh_config.getint('port')
+        username = ssh_config.get('username')
+        password = ssh_config.get('password')
 
-    fifo_path = settings.get('fifo_path')
-    encoder_path = settings.get('encoder_path')
-    source_path = settings.get('source_path')
+        fifo_path = settings.get('fifo_path')
+        encoder_path = settings.get('encoder_path')
+        source_path = settings.get('source_path')
 
-    ssh_client = connect_to_ssh(address, username, password, port)
+        ssh_client = sshConnection(address, username, password, port)
 
-    if ssh_client:
-        run_command(ssh_client, f'screen -dmS MicroRDS bash -c "cd {encoder_path} && ./micrords --ctl {fifo_path}"')
-        print(f"Reading commands from {source_path}")
-        with open(source_path, 'r') as file:
-            for line in file:
-                command = line.strip()
-                if command:
-                    write_to_fifo(ssh_client, fifo_path, command)
-        send_commands_from_file(ssh_client, fifo_path, source_path)
+        if ssh_client:
+            commandRunning(ssh_client, f'screen -dmS MicroRDS bash -c "cd {encoder_path} && ./micrords --ctl {fifo_path}"')
+            try:
+                print(f"Reading commands from {source_path}")
+                with open(source_path, 'r') as file:
+                    for line in file:
+                        command = line.strip()
+                        if command:
+                            fifoWriting(ssh_client, fifo_path, command)
+
+            except FileNotFoundError:
+                print(f"Error: File '{source_path}' not found.")
+                closeAll()
+            fileCommands(ssh_client, fifo_path, source_path)
+
+    except KeyboardInterrupt:
+        closeAll()
