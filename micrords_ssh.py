5b8aa0dcbbe9eb8014efa6347be28f58e1f93ac9
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-03-02 16:25:53 +0100

    Update micrords_ssh.py

--- micrords_ssh.py
+++ micrords_ssh.py
@@ -4,12 +4,6 @@ from paramiko import AutoAddPolicy, SSHClient
 from configparser import ConfigParser
 from time import sleep
 
-def closeAll():
-    print("Closing the screen session and connection...")
-    commandRunning(ssh_client, 'screen -S MicroRDS -X quit')
-    ssh_client.close()
-    exit()
-
 def sshConnection(address, username, password, port):
     ssh_client = SSHClient()
     ssh_client.set_missing_host_key_policy(AutoAddPolicy())
@@ -27,6 +21,12 @@ def commandRunning(ssh_client, command):
     stdin, stdout, stderr = ssh_client.exec_command(command)
     print(stdout.read().decode())
     print(stderr.read().decode())
+
+def closeAll():
+    print("Closing the screen session and connection...")
+    commandRunning(ssh_client, 'screen -S MicroRDS -X quit')
+    ssh_client.close()
+    exit()
 
 def fifoWriting(ssh_client, fifo_path, data):
     try:
@@ -56,7 +56,7 @@ def fileCommands(ssh_client, fifo_path, source_path):
                             line = line.strip()
                             if line:
                                 fifoWriting(ssh_client, fifo_path, line)
-            sleep(1)
+            sleep(0.1)
 
     except FileNotFoundError:
         print(f"Error: File '{source_path}' not found.")
@@ -68,23 +68,57 @@ def fileCommands(ssh_client, fifo_path, source_path):
 
 if __name__ == "__main__":
     try:
-        exe_dir = os.path.dirname(executable)
-        config_path = os.path.join(exe_dir, 'config.conf')
+        exeDir = os.path.dirname(executable)
+        configPath = os.path.join(exeDir, 'config.conf')
+        configExists = os.path.exists(configPath)
 
-        config = ConfigParser()
-        config.read(config_path)
+        if not configExists:
+            print("Config file not detected. Generating...")
+            with open(configPath, 'a') as file:
+                file.write(r"""[SSH]
+# The address of the SSH server. It can be either IP, or hostname of the server.
+address = 192.168.0.10
 
-        ssh_config = config['SSH']
-        settings = config['Settings']
+# The port that SSH server is running on.
+port = 22
 
-        address = ssh_config.get('address')
-        port = ssh_config.getint('port')
-        username = ssh_config.get('username')
-        password = ssh_config.get('password')
+# Username that will be used to connect to the SSH server.
+username = changeme
 
-        fifo_path = settings.get('fifo_path')
-        encoder_path = settings.get('encoder_path')
-        source_path = settings.get('source_path')
+# Password that will be used to connect to the SSH server.  
+password = changeme
+
+[Settings]
+# This is the path to the MicroRDS executable file directory.
+encoder_path = /home/username/MicroRDS/src/
+
+# This is the local file that contains the commands to be sent.
+source_path = C:\Users\username\Documents\rds.txt
+
+# This is the remote FIFO file that is used with MicroRDS.
+fifo_path = /home/username/MicroRDS/scripts/rds_fifo""")
+            print("Config file generated, adjust it to your needs and re-run the program.")
+            exit()
+
+        try:
+            config = ConfigParser()
+            config.read(configPath)
+
+            ssh_config = config['SSH']
+            settings = config['Settings']
+
+            address = ssh_config.get('address')
+            port = ssh_config.getint('port')
+            username = ssh_config.get('username')
+            password = ssh_config.get('password')
+
+            fifo_path = settings.get('fifo_path')
+            encoder_path = settings.get('encoder_path')
+            source_path = settings.get('source_path')
+
+        except KeyError:
+            print("Error: Syntax errors found in the config file.")
+            exit()
 
         ssh_client = sshConnection(address, username, password, port)
 
